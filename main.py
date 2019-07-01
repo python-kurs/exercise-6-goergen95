df9dfb2f90f25a37e8df3b06402b69558e6ec556
goergen95 <darius2402@web.de>
2019-07-01 18:07:07 +0200

    solution excercise 6

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 # Exercise 6
 from pathlib import Path
+import satpy
+from pyresample.geometry import AreaDefinition
+from utils import createResArea
 
 input_dir  = Path("data")
-
+input_dir.mkdir(parents=True,exist_ok=True)
+plot_dir = Path("plots")
+plot_dir.mkdir(parents=True,exist_ok=True)
 # 1. Read the Scene that you downloaded from the data directory using SatPy. [2P]
+files = satpy.find_files_and_readers(base_dir=input_dir)
+scn = satpy.Scene(files)
 
 # 2. Load the composites "natural_color" and "convection" [2P]
+scn.load(["natural_color","convection"])
 
 # 3. Resample the fulldisk to the Dem. Rep. Kongo and its neighbours [4P] 
 #    by defining your own area in Lambert Azimuthal Equal Area. 
@@ -15,4 +23,16 @@ input_dir  = Path("data")
 #      - projection x/y coordinates of lower left: -15E5
 #      - projection x/y coordinates of upper right: 15E5 
 
+# resample to defined area (using function from utils)
+def_area = createResArea(area_ID="Kongo",
+                         description="The Kongo and neighbouring countries in Lambert Equal Area projection",
+                         proj_id="LambertEqualArea",
+                         proj_short="laea",
+                         lon_0=23,lat_0=-3,
+                         width=500,height=500,
+                         llC=-15E5,urC=15E5)
+scn_local = scn.resample(def_area)
+
 # 4. Save both loaded composites of the resampled Scene as simple png images. [2P]
+scn_local.show("natural_color").save(plot_dir / Path("natural_color.png"))
+scn_local.show("convection").save(plot_dir / Path("convection.png"))
--- utils.py
+++ utils.py
@@ -1,4 +1,36 @@
 # utility functions
-
+from pyresample.geometry import AreaDefinition
 # Use this file for all functions you create and might want to reuse later.
 # Import them in the `main.py` script
+def createResArea(area_ID:str,description:str,proj_id:str,proj_short:str,lon_0:int,lat_0:int,width:int,height:int,llC:int,urC:int):
+    '''
+    A function that let's you define an area object used to resample with satpy.
+    
+    parameters:
+    ---------------------
+    areaID: String with a precise title of the defined area.
+    
+    description: String with a more elborative description of the definend area
+    
+    proj_id: String with the long name of the desired projection.
+
+    proj_short: String with the short name of the desired projection.
+    
+    lon_0: Integer specifiying the longitude value of the center position of the specified projection.
+    
+    lat_0: Integer specifiying the latitude value of the center position of the specified projection.
+    
+    width/height: Integers specifiying the desired number of pixels in width/height.
+    
+    llC: X and Y coordinate of the lower-left-Corner of the pixel (only equal values of x and y are currently supported.)
+    
+    urC: X and Y coordinate of the upper-right-Corner of the pixel (only equal values of x and y are currently supported.)
+    '''
+    proj_dict = {"proj":proj_short,"lon_0":str(lon_0),"lat_0":str(lat_0)}
+    llx = llC
+    lly = llC
+    urx = urC
+    ury = urC
+    area_extent = (llx,lly,urx,ury)
+    area_def = AreaDefinition(area_ID, proj_id, description, proj_dict, width, height, area_extent)
+    return(area_def)
